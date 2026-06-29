8dc54729c11b2e92ae9ad8307d4c8b5d2ea91791
Atharva Darvekar <darvekaratharva@gmail.com>
2026-06-29 11:15:04 +0530

    gallary changes

--- components/GallerySection.tsx
+++ components/GallerySection.tsx
@@ -46,7 +46,7 @@ export default function GallerySection({ images = [] }: Props) {
             </p>
             <div className="mt-10 w-12 h-0.5 rounded-full bg-blue-600" />
           </div>
-
+ 
           {images.length > 0 ? (
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
               {images.map((image) => {
